f0a474d1b5df6c52fac1c518fc74c0cd92718c5f
Thais Palma de Brito <britop.tech@gmail.com>
2025-08-19 12:55:21 -0700

    Update budget_app_project.py

--- budget_app_project.py
+++ budget_app_project.py
@@ -27,20 +27,19 @@ class Category:
     def __str__(self):
         output = []  # Create a list to store the output elements
 
-        # Create a title line of 30 characters where the name of 
-        # the category is centered in a line of '*' characters,
-        # and add it to the the output list
+        # Create a title line of 30 characters where the name of the category is centered 
+        # in a line of '*' characters, and append it to the the output list
         output.append((f'{self.category_type}').center(30, '*'))
         
         total = 0
-        # Iterate over self.ledger and add each item's description and amount to the output list
+        # Iterate over self.ledger and append each item's description and amount to the output list
         for item in self.ledger:
             # Slice the first 23 characters, in case the description is too long
             # Right-align the amount with two decimal places and display a max of seven characters
             output.append(f"{item['description'][:23]:<23}{item['amount']:>7.2f}")
             total += item['amount']
 
-        # Add the formatted total to the output list
+        # Append the formatted total to the output list
         output.append(f'Total: {total:.2f}')
 
         # Join all elements of the output list with newline characters and return the result
@@ -52,7 +51,7 @@ class Category:
     def deposit(self, amount, description=''):
         self.ledger.append({'amount': amount, 'description': description})
     
-    # If enough funds, withdraws an amount from the ledger
+    # If sufficient funds, withdraws an amount from the ledger
     def withdraw(self, amount, description=''):
         if self.check_funds(amount):
             self.ledger.append({'amount': -amount, 'description': description})
@@ -98,7 +97,7 @@ def create_spend_chart(categories):
     output_lines.append('Percentage spent by category')
 
     # Compute the total amount spent per category and the grand total from all categories
-    spent_by_category = [] # Create a list to store the total spent by cateogry
+    spent_by_category = [] # Create a list to store the total spent per cateogry
     grand_total = 0
     for category in categories:
         total_per_category = sum(-item['amount'] for item in category.ledger if item['amount'] < 0)
@@ -111,7 +110,7 @@ def create_spend_chart(categories):
         percent = (item['spent'] / grand_total)*100
 
         # Round down the height of each bar to the nearest 10
-        # And add the percentage of each category to spent_by_category
+        # Add the percentage of each category to the spent_by_category list
         item['percent'] = math.floor(percent/10)*10
 
     # Format the percentage spent by category
@@ -133,7 +132,7 @@ def create_spend_chart(categories):
         chart_width = 5 + len(spent_by_category) * 3
         output_lines.append(lines.ljust(chart_width))  # Left-justify, padding it with spaces
 
-    # Add the horizontal dashed line to the output_lines list
+    # Append the horizontal dashed line to the output_lines list
     # Each category column takes up three characters (one space + 'o' + another space)
     # +1 for extra '-' to make the horizontal line below the bars with two spaces past the final bar
     output_lines.append((' '*4) + '-' * (len(spent_by_category) * 3 + 1))
@@ -150,9 +149,9 @@ def create_spend_chart(categories):
                 row += name[n] + (' '*2)
             else:
                 row += (' '*3)
-        output_lines.append(row.ljust(chart_width))  
+        output_lines.append(row.ljust(chart_width))  # Append the formatted rows to the output_lines list  
 
-    # Join all elements of the output_lines list with newline characters and return the result
+    # Join all elements of the output_lines list with newline characters and return the chart
     return '\n'.join(output_lines)
 
 ### Sample Tests
@@ -182,6 +181,7 @@ print(food)
 # Generate and print the spending chart
 chart = create_spend_chart([food, clothing, auto])
 print(chart)
+
 
 
 
